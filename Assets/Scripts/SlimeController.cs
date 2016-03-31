9ad53430c8fc020c90bd46fe83b5bdb521094943
Eduardo Pose <dudupose8@gmail.com>
2016-03-31 18:04:20 -0300

    Added files via upload
    Created a new script for managing the slime status(passive,aggressive) and made some changes to the slimeController to make the slimes move towards the player when aggressive. =D

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -16,57 +16,71 @@ public class SlimeController : MonoBehaviour
     private Rigidbody2D _myRigidbody;
     private Vector3 _moveDirection;
 
+	private slimeStatus slimestatus; // reference to the slimestatus script (that says if the slime is angry)
+
     // Use this for initialization
     private void Start()
     {
         _myRigidbody = GetComponent<Rigidbody2D>();
 
-        //_timeBetweenMoveCounter = timeBetweenMove;
-        //_timeToMoveCounter = timeToMove;
+     //   _timeBetweenMoveCounter = timeBetweenMove;
+     //   _timeToMoveCounter = timeToMove;
 
         _timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
         _timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
+
+		slimestatus = GetComponent<slimeStatus>(); // getting slimestatus script
+		_thePlayer = GameObject.Find("Player"); // the player gameobject
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (_moving)
-        {
-            _timeToMoveCounter -= Time.deltaTime;
-            _myRigidbody.velocity = _moveDirection;
+       if(slimestatus.isAggressive == false){ // if the slime is not aggressive... (i did this because if not, the slime would continue to move normally after becoming aggressive)
 
-            if (_timeToMoveCounter < 0f)
-            {
-                _moving = false;
-                //_timeBetweenMoveCounter = timeBetweenMove;
-                _timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
-            }
-        }
-        else
-        {
-            _timeBetweenMoveCounter -= Time.deltaTime;
-            _myRigidbody.velocity = Vector2.zero;
+			if (_moving)
+		    {
+		        _timeToMoveCounter -= Time.deltaTime;
+		        _myRigidbody.velocity = _moveDirection;
 
-            if (_timeBetweenMoveCounter < 0f)
-            {
-                _moving = true;
-                //_timeToMoveCounter = timeToMove;
-                _timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
+		        if (_timeToMoveCounter < 0f)
+		        {
+		            _moving = false;
+		       //     _timeBetweenMoveCounter = timeBetweenMove;
+		            _timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+		        }
+		    }
+		    else
+		    {
+		        _timeBetweenMoveCounter -= Time.deltaTime;
+		        _myRigidbody.velocity = Vector2.zero;
 
-                _moveDirection = new Vector3(Random.Range(-1, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
-            }
-        }
+		        if (_timeBetweenMoveCounter < 0f)
+		        {
+		            _moving = true;
+		        //    _timeToMoveCounter = timeToMove;
+		            _timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
 
-        if (_reloading)
-        {
-            waitToReload -= Time.deltaTime;
-            if (waitToReload < 0)
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-                _thePlayer.SetActive(true);
-            }
-        }
+		            _moveDirection = new Vector3(Random.Range(-1, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
+		        }
+		    }
+
+		    if (_reloading)
+		    {
+		        waitToReload -= Time.deltaTime;
+		        if (waitToReload < 0)
+		        {
+		            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		            _thePlayer.SetActive(true);
+		        }
+		    }
+		}else{ // if the slime is aggressive...
+
+			transform.position = Vector3.MoveTowards(transform.position,_thePlayer.transform.position,moveSpeed * Time.deltaTime); // move towards the player at the moveSpeed speed
+
+
+		}
+
     }
 
     void OnCollisionEnter2D(Collision2D other)
