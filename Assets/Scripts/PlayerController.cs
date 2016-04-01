26eaae9fbd8a978e3b8ffb4290feeb711e3f61ba
PezeM <pezemos@gmail.com>
2016-04-02 01:08:27 +0200

    Added gold pickup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     #region Variables
     public float moveSpeed;
@@ -24,7 +25,7 @@ public class PlayerController : MonoBehaviour {
 
     #endregion  
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         _animator = GetComponent<Animator>();
         _myRigidbody = GetComponent<Rigidbody2D>();
@@ -41,27 +42,27 @@ public class PlayerController : MonoBehaviour {
         }
 
     }
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	    _playerMoving = false;
 
-	    if (!_attacking)
-	    {
-	        
+    // Update is called once per frame
+    void Update()
+    {
+        _playerMoving = false;
+
+        if (!_attacking)
+        {
+
             // Moving left and right
-	        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
-	        {
-	            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+            {
+                //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
                 _myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * currentMoveSpeed, _myRigidbody.velocity.y);
-	            _playerMoving = true;
+                _playerMoving = true;
                 lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
-	        }
+            }
 
             // Moving up and down
             if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-	        {
+            {
                 //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
                 _myRigidbody.velocity = new Vector2(_myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * currentMoveSpeed);
                 _playerMoving = true;
@@ -69,42 +70,42 @@ public class PlayerController : MonoBehaviour {
             }
 
             // Stoping our player moving left and right when we dont click any button
-	        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
-	        {
-	            _myRigidbody.velocity = new Vector2(0f, _myRigidbody.velocity.y);
-	        }
+            if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+            {
+                _myRigidbody.velocity = new Vector2(0f, _myRigidbody.velocity.y);
+            }
 
             if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
             {
-                _myRigidbody.velocity = new Vector2(_myRigidbody.velocity.x,0f);
+                _myRigidbody.velocity = new Vector2(_myRigidbody.velocity.x, 0f);
             }
 
             // Attacking
-	        if (Input.GetKeyDown(KeyCode.F))
-	        {
-	            _attackTimeCounter = attackTime;
-	            _attacking = true;
-	            _myRigidbody.velocity = Vector2.zero;
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                _attackTimeCounter = attackTime;
+                _attacking = true;
+                _myRigidbody.velocity = Vector2.zero;
                 _animator.SetBool("Attack", true);
-	        }
+            }
 
             // Moving in diagonal directions
-	        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
-	        {
-	            currentMoveSpeed = moveSpeed * diagonalMoveModifier;
-	        }
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
+            {
+                currentMoveSpeed = moveSpeed * diagonalMoveModifier;
+            }
         }
 
-	    if (_attackTimeCounter > 0)
-	    {
-	        _attackTimeCounter -= Time.deltaTime;
-	    }
+        if (_attackTimeCounter > 0)
+        {
+            _attackTimeCounter -= Time.deltaTime;
+        }
 
-	    if (_attackTimeCounter <= 0)
-	    {
-	        _attacking = false;
+        if (_attackTimeCounter <= 0)
+        {
+            _attacking = false;
             _animator.SetBool("Attack", false);
-	    }
+        }
 
         // Setting right animations
         _animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
@@ -112,5 +113,14 @@ public class PlayerController : MonoBehaviour {
         _animator.SetBool("PlayerMoving", _playerMoving);
         _animator.SetFloat("LastMoveX", lastMove.x);
         _animator.SetFloat("LastMoveY", lastMove.y);
-	}
+    }
+
+    // check if player gets in contact with goldcoins
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "goldCoins(Clone)")
+        {
+            Destroy(col.gameObject);
+        }
+    }
 }
--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -17,35 +17,24 @@ public class SlimeController : MonoBehaviour
     private Vector3 moveDirection;
     private SlimeStatus slimeStatus; // reference to the slimestatus script (that says if the slime is angry)
 
-	private slimeStatus slimestatus; // reference to the slimestatus script (that says if the slime is angry)
-
     // Use this for initialization
     private void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
 
-     //   _timeBetweenMoveCounter = timeBetweenMove;
-     //   _timeToMoveCounter = timeToMove;
+        //_timeBetweenMoveCounter = timeBetweenMove;
+        //_timeToMoveCounter = timeToMove;
 
-<<<<<<< HEAD
         timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
         timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
 
         slimeStatus = GetComponent<SlimeStatus>(); // getting slimestatus script
         thePlayer = GameObject.Find("Player"); // the player gameobject
-=======
-        _timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
-        _timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
-
-		slimestatus = GetComponent<slimeStatus>(); // getting slimestatus script
-		_thePlayer = GameObject.Find("Player"); // the player gameobject
->>>>>>> refs/remotes/origin/master
     }
 
     // Update is called once per frame
     private void Update()
     {
-<<<<<<< HEAD
         // When slime is moving his timeToMoveCounter is counting to 0 in this if statement
         if (moving)
         {
@@ -94,52 +83,5 @@ public class SlimeController : MonoBehaviour
                 thePlayer.SetActive(true);
             }
         }
-=======
-       if(slimestatus.isAggressive == false){ // if the slime is not aggressive... (i did this because if not, the slime would continue to move normally after becoming aggressive)
-
-			if (_moving)
-		    {
-		        _timeToMoveCounter -= Time.deltaTime;
-		        _myRigidbody.velocity = _moveDirection;
-
-		        if (_timeToMoveCounter < 0f)
-		        {
-		            _moving = false;
-		       //     _timeBetweenMoveCounter = timeBetweenMove;
-		            _timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
-		        }
-		    }
-		    else
-		    {
-		        _timeBetweenMoveCounter -= Time.deltaTime;
-		        _myRigidbody.velocity = Vector2.zero;
-
-		        if (_timeBetweenMoveCounter < 0f)
-		        {
-		            _moving = true;
-		        //    _timeToMoveCounter = timeToMove;
-		            _timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
-
-		            _moveDirection = new Vector3(Random.Range(-1, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
-		        }
-		    }
-
-		    if (_reloading)
-		    {
-		        waitToReload -= Time.deltaTime;
-		        if (waitToReload < 0)
-		        {
-		            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-		            _thePlayer.SetActive(true);
-		        }
-		    }
-		}else{ // if the slime is aggressive...
-
-			transform.position = Vector3.MoveTowards(transform.position,_thePlayer.transform.position,moveSpeed * Time.deltaTime); // move towards the player at the moveSpeed speed
-
-
-		}
-
->>>>>>> refs/remotes/origin/master
     }
 }
